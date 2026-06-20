1388044fdde274cf0a1041e1ef3c9d4fa11c8ee2
drayerh <107506795+drayerh@users.noreply.github.com>
2026-06-20 21:02:38 +0000

    add list prompts method to MCPClient class

--- mcp_client.py
+++ mcp_client.py
@@ -53,8 +53,8 @@ class MCPClient:
         return await self.session().call_tool(tool_name, tool_input)
 
     async def list_prompts(self) -> list[types.Prompt]:
-        # TODO: Return a list of prompts defined by the MCP server
-        return []
+        result = await self.session().list_prompts()
+        return result.prompts
 
     async def get_prompt(self, prompt_name, args: dict[str, str]):
         # TODO: Get a particular prompt defined by the MCP server
